ad1b96323433d5398a340090e15e4623133c6e20
Abhishek Chauhan <abhishekchauhan1509@gmail.com>
2022-08-14 23:42:09 +0530

    deploy hojaa yaar

--- server.js
+++ server.js
@@ -16,13 +16,15 @@ app.use(express.json());
 //linking the router files
 app.use(require("./router/router"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+
 
 app.get("/contact", (req, res) => { 
   res.send(`Hello Contact world from the server`);
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
