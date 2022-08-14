0e8ad453d01aa5e81a49494b731e8da06f47173b
Abhishek Chauhan <abhishekchauhan1509@gmail.com>
2022-08-14 22:37:38 +0530

    done

--- client/src/components/home/SortBar.js
+++ client/src/components/home/SortBar.js
@@ -1,32 +1,40 @@
 import Button from "@mui/material/Button";
 import React from "react";
+import { AppBar, Toolbar, Box, Typography, styled } from "@mui/material";
 
 const SortBar = ({ products, setFilterProducts }) => {
-
   const togglePrice = (price) => {
-    console.log("tog")
+    // console.log("tog");
     if (price === "low") {
       products.sort((a, b) =>
         a.price.localeCompare(b.price, undefined, { numeric: true })
       );
-    } else if (price === "high") {
+    } else {
       console.log("else");
       products.sort((a, b) =>
         b.price.localeCompare(a.price, undefined, { numeric: true })
       );
-      
     }
-    console.log("before")
-    setFilterProducts([...products]);
-    console.log("after")
+    // console.log("before");
+    setFilterProducts([...products, products]);
+    // console.log("after");
 
-    console.log(products);
+    // console.log(products);
   };
 
   return (
-    <div style={{marginBottom:"20px"}}>
-      <Button  onClick={() => togglePrice("low")}>Price -Low to High</Button>
-      <Button style={{ marginLeft: "20px" }} onClick={() => togglePrice("high")}>Price - High to Low</Button>
+    <div style={{ marginBottom: "20px" }}>
+      Sort By:
+      <Button
+        style={{ marginLeft: "15px" }}
+        onClick={() => togglePrice("high")}
+      >
+        Price - High to Low
+      </Button>
+      <Button style={{ marginLeft: "5px" }} onClick={() => togglePrice("low")}>
+        {" "}
+        Price -Low to High
+      </Button>
     </div>
   );
 };
